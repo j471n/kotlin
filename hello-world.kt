545c92a39766dd54237a856835c7c552cb6ee218
Jatin Sharma (desktop) <55713505+jatinsharma0089@users.noreply.github.com>
2020-07-31 15:20:58 +0530

    hello world program with comments

---
+++ hello-world.kt
@@ -0,0 +1,8 @@
+// hello world
+fun main(args : Array<String>){
+    println("Hello, World!")
+}
+
+
+/*  fun main(args : Array<String>) { ... }
+This is the main function, which is mandatory in every Kotlin application. The Kotlin compiler starts executing the code from the main function.*/
